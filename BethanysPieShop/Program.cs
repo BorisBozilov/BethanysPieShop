8d38b104b25a9cfe8e1cbb350621988cf7760495
BorisBozilov <boris.bozilov@gmail.com>
2022-10-27 14:12:09 +0300

    Added new views and razor pages to the Application.

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
     options.UseSqlServer(
@@ -33,6 +34,8 @@ app.MapDefaultControllerRoute();
 //app.MapControllerRoute(
 //    name: "default",
 //    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 DBInitializer.Seed(app);
 
